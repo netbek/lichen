6e1f007d56bae01a7ff087d111fb7d37f668d30a
Hein Bekker <hein@siyavula.com>
2017-01-18 21:48:31 +0200

    Add support for loading JS data

---
+++ test/data/src/data/near-earth-comets-5.js
@@ -0,0 +1,17 @@
+module.exports = {
+  view: {
+    name: 'Near-Earth Comets - Orbital Elements',
+    columns: [{
+      name: 'Object'
+    }, {
+      name: 'Epoch (TDB)'
+    }],
+    data: [
+      ['1P/Halley', '49400'],
+      ['2P/Encke', '56870'],
+      ['3D/Biela', '-9480'],
+      ['5D/Brorsen', '7440'],
+      ['7P/Pons-Winnecke', '56981']
+    ]
+  }
+};
